b20c2f3aab0fdd94303a3c7a8fcbc51fc5de4a63
ria-19 <riasangwan1999@gmail.com>
2022-09-29 23:17:21 +0530

    "Is Prime?"

--- Basics_Java/GradingSystem.java
+++ Basics_Java/GradingSystem.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class GradingSystem {
+import java.util.Scanner;
+
+public class GradingSystem {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int marks = scn.nextInt();
+        String ans = "below par";
+        // Write your code here
+        if(marks > 90){
+            ans = "excellent";
+        }
+        else if (marks > 80){
+            ans = "good";
+        }
+        else if (marks > 70){
+            ans = "fair";
+        }
+        else if (marks > 60){
+            ans = "meets expectations";
+        }
+        System.out.println(ans);
+    }
 }
