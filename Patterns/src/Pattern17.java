5cd2888a4f60772084f52f46cb250a913d78a41d
ria-19 <riasangwan1999@gmail.com>
2022-10-02 00:09:19 +0530

    2nd Oct

---
+++ Patterns/src/Pattern17.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int sp = n / 2;
+        int st = 1;
+        for(int i = 1; i <= n; i++){
+           if (i != (n / 2) + 1){
+                for(int j = 1; j <= sp; j++){
+                    System.out.print("\t");
+                }
+
+               for(int j = 1; j <= st; j++){
+                   System.out.print("*\t");
+               }
+           }
+           else{
+               for(int j = 1; j <= n; j++){
+                   System.out.print("*\t");
+               }
+           }
+
+           if (i <= n / 2){
+               st++;
+           }
+           else{
+               st--;
+           }
+            System.out.println();
+        }
+    }
+}
