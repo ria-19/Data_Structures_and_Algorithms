a6beadb5d6155f3f6feb736b86f82c9b44e5e1ad
ria-19 <riasangwan1999@gmail.com>
2022-10-02 12:23:54 +0530

    2nd October

--- Patterns/src/Pattern20.java
+++ Patterns/src/Pattern20.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Pattern20 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++) {
+                if (j == 1 || j == n) {
+                    System.out.print("*\t");
+                } else if (i > n / 2 && (i == j || (i + j) == n + 1)){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
